fbb36fc060aa3033d0530ccf45beb3cfa70c33ea
María Solorzano <mariadelosasolorzanosanchez@gmail.com>
2021-08-25 11:05:27 -0430

    cambios para ver si sirve el borrar registro

--- borrarregistro.php
+++ borrarregistro.php
@@ -40,4 +40,12 @@ if ($_POST['password'] != $password) {
     alert("Producto Eliminado exitosamente");
     window.location.href='persona.html';
 </script>
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
+    </body>
 </html>
